3d9988b3cac6787efc5b75084071d035e5bf5ff7
Jesse <jessepirrotta@gmail.com>
2023-03-13 23:55:26 -0400

    btn btn-primary

--- components/levelDown.js
+++ components/levelDown.js
@@ -4,7 +4,7 @@ export default function LevelDown() {
     const [powerLevel, setPowerLevel] = useAtom(powerLevelAtom);
     return (
         <>
-            <button onClick={() => setPowerLevel(powerLevel - 1000)}>Level Down</button>
+            <button class="btn btn-primary" onClick={() => setPowerLevel(powerLevel - 1000)}>Level Down</button>
 
         </>
     );
--- components/levelUp.js
+++ components/levelUp.js
@@ -4,7 +4,7 @@ export default function LevelUp() {
     const [powerLevel, setPowerLevel] = useAtom(powerLevelAtom);
     return (
         <>
-            <button onClick={() => setPowerLevel(powerLevel + 1000)}>Level Up</button>
+            <button class="btn btn-primary" onClick={() => setPowerLevel(powerLevel + 1000)}>Level Up</button>
         </>
     );
 
