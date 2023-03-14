008cd889e4a94c73dd76f6bad016f58d1d2c50d6
Jesse <jessepirrotta@gmail.com>
2023-03-13 23:45:57 -0400

    funny 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Link
+    from "next/link"
+export default function Lost() {
+    return <>
+        <div align='center'>
+            <h1>WHERE ARE YOU????</h1>
+            <img src="https://i.pinimg.com/736x/b4/28/db/b428dbfe260b156e9ab22754b922d9fd--last-minute-gifts-the-shorts.jpg"></img>
+            <Link href="/"><h1>Home</h1></Link>
+        </div>
+    </>
+}
