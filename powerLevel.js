566d747a8763856f8b7f88757842b2de6873af67
Jesse <jessepirrotta@gmail.com>
2023-03-13 23:57:07 -0400

    jotai explanation

--- powerLevel.js
+++ powerLevel.js
@@ -1,3 +1,6 @@
+//Create your atom here
+//Atom is like a state.
+
 import { atom } from 'jotai';
 
-export const powerLevelAtom = atom(9000);
+export const powerLevelAtom = atom(9000); //9000 is the initial value of the atom
